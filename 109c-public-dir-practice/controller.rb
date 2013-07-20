c5f3dcab1dfeef8318663d8dccad3416583e4522
Patty Laushman <plaushman@gmail.com>
2013-07-19 18:14:38 -0600

    Did class assignment

--- 109c-public-dir-practice/controller.rb
+++ 109c-public-dir-practice/controller.rb
@@ -1,3 +1,13 @@
 require 'sinatra'
 
-# TODO: add your GET handlers here
+get "/page1" do
+	halt erb(:page1)
+end
+
+get "/page2" do
+	halt erb(:page2)
+end
+	
+get "/page3" do
+	halt erb(:page3)
+end
