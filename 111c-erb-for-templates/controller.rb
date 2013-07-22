063d6e767d1980cbe9731049b95c5ce4c9449470
Patty Laushman <plaushman@gmail.com>
2013-07-22 17:55:22 -0600

    Completed class assignment

--- 111c-erb-for-templates/controller.rb
+++ 111c-erb-for-templates/controller.rb
@@ -13,6 +13,7 @@ end
 
 get "/reposts/2" do
   @title = "How to support WebP images, save bandwidth and improve user performance"
-  @summary = "The WebP format is another great tool to achieve our goals. With Cloudinarys on-the-fly, cloud-based WebP image format conversion, this becomes much simpler.
+  @summary = "The WebP format is another great tool to achieve our goals. With Cloudinarys on-the-fly, cloud-based WebP image format conversion, this becomes much simpler."
+  @link = "http://cloudinary.com/blog/how_to_support_webp_images_save_bandwidth_and_improve_user_performance"
   halt erb(:repost1)
 end
