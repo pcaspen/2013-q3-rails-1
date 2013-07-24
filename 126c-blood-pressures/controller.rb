9413aa9ab4a7b910424733d88078fd489d75cdba
Patty Laushman <plaushman@gmail.com>
2013-07-24 11:46:36 -0600

    Classwork

--- 126c-blood-pressures/controller.rb
+++ 126c-blood-pressures/controller.rb
@@ -4,3 +4,11 @@ get "/" do
   @patients = Patient.all
   halt erb(:root)
 end
+
+post "/new" do
+  post         = Patient.new
+  post.systolic  = params["systolic"]
+  post.diastolic = params["diastolic"]
+  post.save!
+  redirect "/"
+end
