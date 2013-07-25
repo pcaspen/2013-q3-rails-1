99b6ab710afe672fdf950aaaf11dbe33616db569
Patty Laushman <plaushman@gmail.com>
2013-07-25 11:03:57 -0600

    Reviewed class assignment and did it again

--- 126c-blood-pressures/controller.rb
+++ 126c-blood-pressures/controller.rb
@@ -4,11 +4,3 @@ get "/" do
   @patients = Patient.all
   halt erb(:root)
 end
-
-post "/new" do
-  post         = Patient.new
-  post.systolic  = params["systolic"]
-  post.diastolic = params["diastolic"]
-  post.save!
-  redirect "/"
-end
