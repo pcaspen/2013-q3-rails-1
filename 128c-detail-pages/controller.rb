e04ce0ca8490ca4bcbd282a15a96674509e6bb0c
Patty Laushman <plaushman@gmail.com>
2013-07-28 15:00:40 -0600

    Practiced class assignment again

--- 128c-detail-pages/controller.rb
+++ 128c-detail-pages/controller.rb
@@ -1,7 +1,7 @@
 require '../dvc-sinatra.rb'
 
 get "/" do
-  # TODO second: add code here to load @apartments
+  @apartments = Apartment.all
   halt erb(:index)
 end
 
@@ -11,7 +11,35 @@ get "/apartments/:id" do
   halt erb(:show)
 end
 
-# get "/apartments/2" do
-#   @apartment = Apartment.find(2)
+
+
+# require '../dvc-sinatra.rb'
+
+# get "/" do
+#   @apartments = Apartment.all
+#   halt erb(:index)
+# end
+
+# get "/apartments/:id" do
+#   id = params[:id]
+#   @apartment = Apartment.find(id)
 #   halt erb(:show)
+# end
+
+
+# require '../dvc-sinatra.rb'
+
+# get "/" do
+#   # TODO second: add code here to load @apartments
+#   halt erb(:index)
+# end
+
+# get "/apartments/1" do
+#   # TODO first: add code here to load @apartment
+#   halt erb(:show1)
+# end
+
+# get "/apartments/2" do
+#   # TODO first: add code here to load @apartment
+#   halt erb(:show2)
 # end
