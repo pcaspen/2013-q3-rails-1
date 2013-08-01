712fe7e7a6b5a91d13abfb3f4999a9813438eebd
Patty Laushman <plaushman@gmail.com>
2013-07-31 18:15:59 -0600

    Did work in class

--- 134c-form-helpers/controller.rb
+++ 134c-form-helpers/controller.rb
@@ -3,3 +3,12 @@ require '../dvc-sinatra.rb'
 get "/" do
   halt erb(:root)
 end
+
+post "/" do
+	new_email	= Email.new
+    new_email.subject = params[:subject]
+    new_email.body	  = params[:body]
+  new_email.save!
+  redirect "/"
+end
+
--- 135c-edit-blood-pressures/controller.rb
+++ 135c-edit-blood-pressures/controller.rb
@@ -9,3 +9,11 @@ get "/patients/:id" do
   @patient = Patient.find(params[:id])
   halt erb(:edit)
 end
+
+post "/patients/:id" do
+	@patient = Patient.find(params[:id])
+	@patient.systolic = params[:systolic]
+	@patient.diastolic = params[:diastolic]
+	@patient.save!
+	redirect "/"
+end
