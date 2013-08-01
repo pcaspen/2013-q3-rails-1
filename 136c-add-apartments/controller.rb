3ffc693620db61677230cd8b6c24542db994e5ec
Patty Laushman <plaushman@gmail.com>
2013-08-01 13:06:49 -0600

    Did homework

--- 136c-add-apartments/controller.rb
+++ 136c-add-apartments/controller.rb
@@ -4,3 +4,13 @@ get "/" do
   @apartments = Apartment.all
   halt erb(:root)
 end
+
+post "/" do
+  apartment                 = Apartment.new
+  apartment.address         = params["address"]
+  apartment.rent_per_month  = params["rent_per_month"]
+  apartment.does_allow_pets = params["does_allow_pets"]
+  apartment.url             = params["url"]
+  apartment.save!
+  redirect "/"
+end
