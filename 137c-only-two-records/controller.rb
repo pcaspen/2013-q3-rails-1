128264dce4b40a152ebc339e9282dc025eb3a81a
Patty Laushman <plaushman@gmail.com>
2013-08-01 13:18:01 -0600

    Got line 1 to edit correctly

--- 137c-only-two-records/controller.rb
+++ 137c-only-two-records/controller.rb
@@ -10,10 +10,19 @@ get "/line1" do
   halt erb(:edit)
 end
 
+post "/line1" do
+  line = 	BraceletLine.find(1)
+  line.font_size = params[:font_size]
+  line.text = params[:text]
+  line.save!
+  redirect "/"
+end  
+
 get "/line2" do
   @bracelet_line = BraceletLine.find(2)
   halt erb(:edit)
 end
+
 
 
 # require '../dvc-sinatra.rb'
