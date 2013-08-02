cf82fb62dde65897a0e77808a6e9f754954e4b44
Patty Laushman <plaushman@gmail.com>
2013-08-01 18:08:07 -0600

    Got it working!

--- 138c-goat-crud-no-delete/controller.rb
+++ 138c-goat-crud-no-delete/controller.rb
@@ -10,7 +10,27 @@ get "/new-goat" do
   halt erb(:new)
 end
 
+post "/new-goat" do
+  goat 						= RacingGoat.new
+  goat.name					= params[:name]
+  goat.gender				= params[:gender]
+  goat.best_100m_time		= params[:best_100m_time]
+  goat.dietary_preference 	= params[:dietary_preference]
+  goat.save!
+  redirect "/"
+end
+
 get "/:id" do
   @goat = RacingGoat.find(params[:id])
   halt erb(:edit)
+end
+
+post "/:id" do
+  goat 						= RacingGoat.find(params[:id])
+  goat.name					= params[:name]
+  goat.gender				= params[:gender]
+  goat.best_100m_time		= params[:best_100m_time]
+  goat.dietary_preference 	= params[:dietary_preference]
+  goat.save!
+  redirect "/"
 end
