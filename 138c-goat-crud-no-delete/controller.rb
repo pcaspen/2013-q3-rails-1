ecccd1c9524708cb318d022fa6b139caf6fb3337
Patty Laushman <plaushman@gmail.com>
2013-08-01 18:18:33 -0600

    Adjusted formatting slightly of controller file

--- 138c-goat-crud-no-delete/controller.rb
+++ 138c-goat-crud-no-delete/controller.rb
@@ -11,10 +11,10 @@ get "/new-goat" do
 end
 
 post "/new-goat" do
-  goat 						= RacingGoat.new
-  goat.name					= params[:name]
-  goat.gender				= params[:gender]
-  goat.best_100m_time		= params[:best_100m_time]
+  goat 						          = RacingGoat.new
+  goat.name					        = params[:name]
+  goat.gender				        = params[:gender]
+  goat.best_100m_time		    = params[:best_100m_time]
   goat.dietary_preference 	= params[:dietary_preference]
   goat.save!
   redirect "/"
@@ -26,11 +26,11 @@ get "/:id" do
 end
 
 post "/:id" do
-  goat 						= RacingGoat.find(params[:id])
-  goat.name					= params[:name]
-  goat.gender				= params[:gender]
-  goat.best_100m_time		= params[:best_100m_time]
-  goat.dietary_preference 	= params[:dietary_preference]
+  goat 						         = RacingGoat.find(params[:id])
+  goat.name					       = params[:name]
+  goat.gender				       = params[:gender]
+  goat.best_100m_time		   = params[:best_100m_time]
+  goat.dietary_preference  = params[:dietary_preference]
   goat.save!
   redirect "/"
 end
