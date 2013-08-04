85d1848949161a18db51a1c6bc7e6b3403177f9f
Patty Laushman <plaushman@gmail.com>
2013-08-04 10:53:10 -0600

    Finished homework assignment

--- 140c-arithmetic-quiz/controller.rb
+++ 140c-arithmetic-quiz/controller.rb
@@ -5,7 +5,12 @@ get "/" do
 end
 
 get "/1" do
-  halt erb(:question1)
+  @question_num = "1"
+  @question = "What is 8 + 5?"
+  @answer_a = "15"
+  @answer_b = "40"
+  @answer_c = "13"
+  halt erb(:question)
 end
 
 post "/1" do
@@ -17,7 +22,12 @@ post "/1" do
 end
 
 get "/2" do
-  halt erb(:question2)
+  @question_num = "2"
+  @question = "What is 5 - 3?"
+  @answer_a = "2"
+  @answer_b = "3"
+  @answer_c = "8"	
+  halt erb(:question)
 end
 
 post "/2" do
