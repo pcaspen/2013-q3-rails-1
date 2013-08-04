57ae045b16b6d68aae8fc2811ef537874b926c68
Patty Laushman <plaushman@gmail.com>
2013-08-04 16:19:44 -0600

    Got username to populate only if correct

--- 139c-hardcoded-login/controller.rb
+++ 139c-hardcoded-login/controller.rb
@@ -7,13 +7,12 @@ end
 post "/" do
   if params[:username] != "amy"
    	@error = "Uknown username"
-   	@old_username = params[:username]
   	halt erb(:login)
   end
 
   if params[:password] != "123"
    	@error = "Uknown password"
-   	@old_username = params[:username]
+    @old_username = params[:username]
   	halt erb(:login)
   else
 
