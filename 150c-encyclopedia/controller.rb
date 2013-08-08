146bc9c48536864404918b1a69b41435b32aba86
Patty Laushman <plaushman@gmail.com>
2013-08-08 13:19:06 -0600

    Finished homework assignment

--- 150c-encyclopedia/controller.rb
+++ 150c-encyclopedia/controller.rb
@@ -5,9 +5,8 @@ get "/" do
   halt erb(:index)
 end
 
-get "/entries/:id" do
-  id = params[:id]
-  @entry = EncyclopediaEntry.find(id)
+get "/:word" do
+  word = params[:word]
+  @entry = EncyclopediaEntry.where(word: params[:word]).first
   halt erb(:show)
 end
-
