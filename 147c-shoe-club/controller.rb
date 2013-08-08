eddf484b8a307bb4af8cd825a7b4d79b036d459d
Patty Laushman <plaushman@gmail.com>
2013-08-07 18:10:18 -0600

    Finished and reviewed assignment

--- 147c-shoe-club/controller.rb
+++ 147c-shoe-club/controller.rb
@@ -8,55 +8,47 @@ end
 get "/sign_up" do
   @customer = Customer.first
   halt erb(:sign_up)
-end  
+end
 
 post "/sign_up" do
   @customer = Customer.first
-  
   if params[:commit] == "Go back"
     redirect "/"
-  elsif params[:commit] == "Continue"
+  elsif params[:commit] == "Continue"  
     @customer.first_name = params[:first_name]
     @customer.last_name = params[:last_name]
     if @customer.save
       redirect "/shipping"
     else
       halt erb(:sign_up)
-    end
+    end  
   end
-end 
+end
 
 get "/shipping" do
   @customer = Customer.first
   @u_s_states = USState.order(:name).all
   halt erb(:shipping)
-end 
+end
 
 post "/shipping" do
   @customer = Customer.first
   @u_s_states = USState.order(:name).all
-  
-  if params[:ship_speed] == nil
-    @customer.ship_speed = "" # just so validation fails
-    else
-    @customer.ship_speed = params[:ship_speed]
-  end
-  
   if params[:commit] == "Go back"
     redirect "/sign_up"
-  elsif params[:commit] == "Continue"
-    @customer.ship_speed = params[:ship_speed]
-    @customer.ship_address1 = params[:ship_address1]
-    @customer.ship_city = params[:ship_city]
-    @customer.ship_state = params[:ship_state]
-    @customer.ship_zip_code = params[:ship_zip_code]
+  elsif params[:commit] == "Continue"  
+    @customer.ship_speed    = params[:ship_speed]
+    @customer.ship_address1 = params[:address1]
+    @customer.ship_city     = params[:city]
+    @customer.ship_state    = params[:state]
+    @customer.ship_zip_code = params[:zip_code]
     if @customer.save
-    redirect "/billing" 
+      redirect "/billing"
     else
-    halt erb(:shipping)
-    end
-  end  
-end 
+      halt erb(:shipping)
+    end  
+  end
+end
 
 get "/billing" do
   @customer = Customer.first
@@ -70,30 +62,29 @@ post "/billing" do
   if params[:commit] == "Go back"
     redirect "/shipping"
   elsif params[:commit] == "Continue"
-    if @customer.bill_address_same_as_ship != nil
+    if params[:same_as_ship] != nil  
       @customer.bill_address_same_as_ship = true
-      @customer.bill_address1 = @customer.ship_address1
+      @customer.bill_address1 = @customer.ship_address1 
       @customer.bill_city     = @customer.ship_city
       @customer.bill_state    = @customer.ship_state
       @customer.bill_zip_code = @customer.ship_zip_code
     else
-      @customer.bill_address_same_as_ship = false
-      @customer.bill_address1 = params[:bill_address1]
-      @customer.bill_city     = params[:bill_city]
-      @customer.bill_state    = params[:bill_state]
-      @customer.bill_zip_code = params[:bill_zip_code]
-    end 
+      @customer.bill_address_same_as_ship = false  
+      @customer.bill_address1 = params[:address1]
+      @customer.bill_city     = params[:city]
+      @customer.bill_state    = params[:state]
+      @customer.bill_zip_code = params[:zip_code]
+    end  
     if @customer.save
-      redirect "/review" 
+      redirect "/review"
     else
       halt erb(:billing)
-    end
-  end  
+    end  
+  end
 end
 
 get "/review" do
   @customer = Customer.first
-  @u_s_states = USState.order(:name).all
   halt erb(:review)
 end
 
@@ -101,13 +92,13 @@ post "/review" do
   @customer = Customer.first
   if params[:commit] == "Go back"
     redirect "/billing"
-  elsif params[:commit] == "Start over"
+  elsif params[:commit] == "Start over"  
     @customer.destroy
     Customer.create
     redirect "/"
-  else params[:commit] == "Place order"
-    redirect "/thank_you" 
-  end  
+  elsif params[:commit] == "Place order"
+      redirect "/thank_you"
+  end
 end
 
 get "/thank_you" do
