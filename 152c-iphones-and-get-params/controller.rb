dd1d0231c8527b401d4ea2106308777b978729f6
Patty Laushman <plaushman@gmail.com>
2013-08-08 17:09:26 -0600

    Got it working

--- 152c-iphones-and-get-params/controller.rb
+++ 152c-iphones-and-get-params/controller.rb
@@ -6,23 +6,19 @@ end
 
 get "/listings" do
   model = params[:model]
-  gigs = params[:gigs]
+  gigs = params[:gig]	
   if model != nil
-  	if gigs != nil
-  	  @listings = 
-  	  Listing.where(model: model).where(gigs: gigs).order(:price).all
-  	else
-  	  @listings = 
-  	  Listing.where(model: model).order(:price).all
-    end
-  else
-  	if gigs != nil
-      @listings =
-      Listing.where(gigs: gigs).order(:price).all
+    if gigs != nil
+      @listings = Listing.where(model: model).where(gigs: gigs).order(:price).all 
     else
-      @listings =
-      Listing.order(:price).all
+      @listings = Listing.where(model: model).order(:price).all
     end   
-  end
+  else
+    if gigs != nil
+      @listings = Listing.where(gigs: gigs).order(:price).all 
+    else
+      @listings = Listing.order(:price).all 
+    end
+  end      	
   halt erb(:listings)
 end
