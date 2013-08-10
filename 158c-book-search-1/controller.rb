dc4864c38cb357f185165a3e8867866eb4eab47d
Patty Laushman <plaushman@gmail.com>
2013-08-10 11:33:36 -0600

    Got class assignment working.

--- 158c-book-search-1/controller.rb
+++ 158c-book-search-1/controller.rb
@@ -5,12 +5,24 @@ get "/" do
 end
 
 get "/year/:year" do
-  # TODO: Finish year route by
-  #       - assigning to @books
-  #       - assigning to @header
+  @year = params[:year]
+  @books = Book.where(publication_year: @year).all
+  @header = "Books published in #{@year}"
   halt erb(:index)
 end
 
 # TODO: Write author route
+get "/author/:author" do
+  @author = params[:author]
+  @books = Book.where(author: @author).all
+  @header = "Books published by #{@author}"
+  halt erb(:index)
+end
 
 # TODO: Write topic route
+get "/topic/:topic" do
+  topic = params[:topic]
+  @books = Book.where("title ilike ?", "%#{topic}%").all 
+  @header = "Books published about #{topic}"
+  halt erb(:index)
+end
