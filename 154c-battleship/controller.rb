a3687c52cf7d8d4075a9add0203b447c390483b3
Patty Laushman <plaushman@gmail.com>
2013-08-11 16:46:46 -0600

    Got homework working but need to repeat

--- 154c-battleship/controller.rb
+++ 154c-battleship/controller.rb
@@ -11,6 +11,28 @@ post "/" do
   @guesses = Guess.all
   @rows = [1, 2, 3, 4, 5]
   @cols = ["A", "B", "C", "D", "E"]
+
+  row = params[:row]
+  col = params[:col]
   
-  redirect "/"
+  existing_guess = Guess.where(row: row, col: col).first
+  if existing_guess != nil
+  	@error = "Already guessed that"
+  	halt erb(:grid)
+  else	
+    guess = Guess.new
+    guess.row = row
+    guess.col = col
+
+  ship = Ship.where(row: row, col: col).first
+	if ship != nil
+	  guess.was_hit = true
+	else	
+	  guess.was_hit = false
+    end
+
+  guess.save!
+  
+  redirect "/"  
+  end
 end
