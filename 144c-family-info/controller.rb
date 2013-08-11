8c3dedcfa5075f32dea2d760c7654053855515ff
Patty Laushman <plaushman@gmail.com>
2013-08-10 19:18:36 -0600

    Did homework again

--- 144c-family-info/controller.rb
+++ 144c-family-info/controller.rb
@@ -6,13 +6,14 @@ get "/" do
 end
 
 post "/" do
-  person 					= Person.new
-  person.relation_to_filer 	= params[:relation_to_filer]
-  person.first_name 		= params[:first_name]
-  person.last_name 			= params[:last_name]
-  person.date_of_birth 		= params[:date_of_birth]
-  person.gender 			= params[:gender]
-  person.is_dependent 		= params[:is_dependent] == "on"
-  person.save!
-  redirect "/"
+  @person = Person.new
+  @person.relation_to_filer = params[:relation_to_filer]
+  @person.first_name 		= params[:first_name]
+  @person.last_name 		= params[:last_name]  
+  @person.date_of_birth 	= params[:date_of_birth]
+  @person.gender 			= params[:gender]
+  @person.is_dependent 		= params[:is_dependent] == "on"
+  if @person.save!
+  	redirect "/"
+  end
 end
