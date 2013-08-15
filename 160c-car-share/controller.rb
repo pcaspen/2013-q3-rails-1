d39fa860b652ca51b95b86d524ba19471dfe7ac0
Patty Laushman <plaushman@gmail.com>
2013-08-15 16:19:17 -0600

    Practiced again

--- 160c-car-share/controller.rb
+++ 160c-car-share/controller.rb
@@ -9,52 +9,53 @@ get "/login" do
 end
 
 post "/login" do
-  @username = params[:username]
-  @password = params[:password]
-  @member = Member.where(username: @username).first
-	if @member == nil
-		@error = "Error: Unknown username"
-		halt erb(:login)
-	else
-	  if @member.password != @password
-	  	@error = "Error: Wrong password"
-	  	halt erb(:login)
-	  else	
-	    redirect "/reservations/#{@member.id}"
-	  end
-	end  
-  halt erb(:login)
+  username = params[:username]
+  password = params[:password]
+  
+  @member = Member.where(username: username).first
+  if @member == nil
+    @error = "Unknown username"
+    halt erb(:login)
+  end
+
+  @member = Member.where(password: password).first
+  if @member == nil
+    @error = "Wrong password"
+    @username = username
+    halt erb(:login)
+  end
+  
+  redirect "/reservations/#{@member.id}"
 end
 
 get "/reservations/:member_id" do
   member_id = params[:member_id]
-  @member = Member.find(member_id)
-  @reserved_cars = Car.where(reserving_member_id: @member.id).all
+  @member = Member.where(id: member_id).first
+  @reserved_cars = Car.where(reserving_member_id: member_id).all
   halt erb(:reservations)
-end
+end 
 
 post "/reservations/:member_id" do
   member_id = params[:member_id]
-  member = Member.find(member_id)
+  @member = Member.where(id: member_id).first
 
-	if params[:commit] == "Logout"
-		redirect "/"
-	end
-
-	Car.all.each do |car|
-    if params[:commit] == "Reserve car #{car.id}"
-      car.reserving_member_id = member_id
-      car.save!
-    end
-  end
-
+  if params[:commit] == "Logout"
+    redirect "/"
+  end  
+  
   Car.all.each do |car|
     if params[:commit] == "Return car #{car.id}"
       car.reserving_member_id = nil
       car.save!
+      redirect "/reservations/#{member_id}"
+    end
+  end  
+
+  Car.all.each do |car|
+    if params[:commit] == "Reserve car #{car.id}"
+      car.reserving_member_id = @member.id
+      car.save!
+      redirect "/reservations/#{member_id}"
     end 
   end
-
-  
-  redirect "/reservations/#{member_id}"
-end
+end 
