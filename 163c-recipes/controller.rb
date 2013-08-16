ef611efff8ce30ca9a7eae090695d82ce6aee337
Patty Laushman <plaushman@gmail.com>
2013-08-16 09:00:05 -0600

    Working on homework. Go back to 162 and 163.

--- 163c-recipes/controller.rb
+++ 163c-recipes/controller.rb
@@ -13,6 +13,8 @@ end
 
 get "/by-author/:name" do
   # TODO: assign to @recipes the recipes for this author
+  author_id = Author.where(name: params[:name]).id.first
+  @recipes = Recipe.where(author_id: author_id)
   # TODO: assign to @title "Recipes by _____"
   halt erb(:index)
 end
