7dca16880402a04c972b56a6c5d5363938e67305
Patty Laushman <plaushman@gmail.com>
2013-08-16 18:23:48 -0600

    Did homework again

--- 161c-team-rosters/controller.rb
+++ 161c-team-rosters/controller.rb
@@ -1,35 +1,35 @@
 require '../dvc-sinatra.rb'
 
-get "/" do
-	@teams = Team.all
+get "/" do 	
   halt erb(:teams)
 end
 
-post "/" do
-	if params[:commit] == "Add"
-		new_team = Team.new
-		new_team.color = params[:color]
-		new_team.save!
-	end	
-  halt erb(:teams)
+post "/" do 	
+  if params[:commit] == "Add"
+	  new_team_color = params[:new_team_color]
+	  new_team = Team.new
+	  new_team.color = new_team_color
+	  new_team.save!
+	  redirect "/"
+  end
 end
 
 get "/team/:color" do
-  # WHAT DO I HAVE AS PARAMS? team.color (violet) --> team.id (2) --> member.team_id (2)
-  # WHERE DO I NEED TO GO?     pull list of members with member.team_id = 2 
-  color = params[:color]
-  @team = Team.where(color: color).first # pulls out one team object with id and color
-  @members = Member.where(team_id: @team.id).all
+	@team_color = params[:color]
+  @team = Team.where(color: @team_color).first
+  @members = Member.where(team_id: @team.id).all 
   halt erb(:members)
 end
 
 post "/team/:color" do
-  color = params[:color]
-  if params[:commit] == "Add"
-    new_member = Member.new
-    new_member.name = params[:name]
-    new_member.team_id = Team.where(color: color).first.id
-    new_member.save!
-  end
-  redirect "/team/#{color}"
+	@team_color = params[:color]
+	
+	if params[:commit] == "Add"
+		new_member_name = params[:new_member_name]
+		new_member = Member.new
+		new_member.name = new_member_name
+		new_member.team_id = Team.where(color: @team_color).first.id
+		new_member.save!
+		redirect "/team/#{@team_color}"
+	end 
 end
