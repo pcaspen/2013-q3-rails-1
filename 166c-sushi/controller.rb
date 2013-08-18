6b1da482c4b48216c4037f9a9909d3d287458419
Patty Laushman <plaushman@gmail.com>
2013-08-18 13:34:27 -0600

    Finished homework

--- 166c-sushi/controller.rb
+++ 166c-sushi/controller.rb
@@ -10,7 +10,25 @@ get "/place_order" do
 end
 
 post "/place_order" do
-  # TODO: Write this
+  order = Order.new
+  order.table_number = params[:table_number]
+  order.total_price = 0.00
+  order.save!
+
+  Dish.all.each do |dish|
+    quantity = params["quantity_for_dish_#{dish.id}"]
+    if quantity != ""
+      line_item = LineItem.new
+      line_item.dish_id = dish.id
+      line_item.order_id = order.id
+      line_item.quantity = quantity
+      line_item.line_price = dish.price * quantity.to_i
+      order.total_price = order.total_price + line_item.line_price
+      line_item.save!
+    end  
+  end
+  
+  order.save!
 
   redirect "/"
 end
