8b691b628aa6878855c124ea941c0d8b16c92357
Patty Laushman <plaushman@gmail.com>
2013-08-20 17:49:06 -0600

    Did homework

--- 171c-preferences-in-database/controller.rb
+++ 171c-preferences-in-database/controller.rb
@@ -1,13 +1,27 @@
 require '../dvc-sinatra.rb'
 
 get "/" do
-  # TODO: Write this
+  if session[:user_id] != nil
+  	@user = User.find(session[:user_id])
+  else
+  	@user = User.new
+  end	
 
   halt erb(:main)
 end
 
 post "/" do
-  # TODO: Write this
+  if session[:user_id] != nil
+    user = User.find(session[:user_id])
+  else
+    user = User.new
+  end
+
+  user.color = params[:color]
+  user.letter = params[:letter]
+  user.number = params[:number]
+  user.save!
+  session[:user_id] = user.id
 
   redirect "/"
 end
