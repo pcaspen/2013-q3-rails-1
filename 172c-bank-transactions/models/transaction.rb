05a702bcdd6f45f0e402bd5a60f1cefebb82781e
Patty Laushman <plaushman@gmail.com>
2013-08-21 11:43:40 -0600

    Partly done

--- 172c-bank-transactions/models/transaction.rb
+++ 172c-bank-transactions/models/transaction.rb
@@ -1,8 +1,8 @@
 class Transaction < ActiveRecord::Base
   belongs_to :account
 
-  validate :account_id, presence: true
-  validate :date,       presence: true
-  validate :memo,       presence: true
-  validate :amount,     presence: true
+  validates :account_id, presence: true
+  validates :date,       presence: true
+  validates :memo,       presence: true
+  validates :amount,     presence: true
 end
