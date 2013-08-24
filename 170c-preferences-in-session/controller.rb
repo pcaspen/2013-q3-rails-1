d8345bf2a975aa1658a35911ce2d0b6c276c7b48
Patty Laushman <plaushman@gmail.com>
2013-08-24 13:57:52 -0600

    Did assignment again

--- 170c-preferences-in-session/controller.rb
+++ 170c-preferences-in-session/controller.rb
@@ -1,19 +1,20 @@
 require '../dvc-sinatra.rb'
 
 get "/" do
+  # TODO #3: Read each setting from the session
+  #          and store in instance variables.
   @color = session[:color]
   @letter = session[:letter]
   @number = session[:number]
-
   halt erb(:main)
 end
 
 post "/" do
+  # TODO #2: Save each setting the user entered into the session.
+  #          Try raise session.inspect to see if it got saved.
   session[:color] = params[:color]
   session[:letter] = params[:letter]
   session[:number] = params[:number]
-  # TODO #2: Save each setting the user entered into the session.
-  #          Try raise session.inspect to see if it got saved.
 
   redirect "/"
 end
