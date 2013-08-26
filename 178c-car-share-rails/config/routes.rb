84fa5b849ef698a322c1ce9239d4ece3464ae5ec
Patty Laushman <plaushman@gmail.com>
2013-08-26 11:58:56 -0600

    Did class assignments

--- 178c-car-share-rails/config/routes.rb
+++ 178c-car-share-rails/config/routes.rb
@@ -1,56 +1,13 @@
 App::Application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+ 
+  get "/"                          => "main#go_to_login"
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  get "/login"                     => "main#login_get"
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  post "/login"                    => "main#login_post"
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  get "/reservations/:member_id"   => "main#reservations_get"
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  post "/reservations/:member_id"  => "main#reservations_post"
 
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-  
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
--- 178c-car-share-rails/db/seeds.rb
+++ 178c-car-share-rails/db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Car.create(color: "red",   model: "VW Golf")
+Car.create(color: "black", model: "VW Jetta")
+Car.create(color: "white", model: "Toyota Celica")
+
+Member.create(username: "jen", password: "123")
+Member.create(username: "ted", password: "456")
+
+car = Car.where(model: "VW Jetta").first
+car.reserving_member_id = Member.where(username: "jen").first.id
+car.save!
