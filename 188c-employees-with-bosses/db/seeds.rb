b8ca918599c6e4ab542d5fa6c8bda7fd0d178128
Patty Laushman <plaushman@gmail.com>
2013-09-02 20:30:53 -0600

    Homework

---
+++ 188c-employees-with-bosses/db/seeds.rb
@@ -0,0 +1,4 @@
+Employee.create! name: "Fred", boss_employee_id: nil
+Employee.create! name: "Sal",  boss_employee_id: 1
+Employee.create! name: "Phil", boss_employee_id: 1
+Employee.create! name: "Jim",  boss_employee_id: 2
